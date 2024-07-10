047f5ac9c0533b5ddd1393ced8e17b919f461fdf
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-10 10:27:53 +0200

    ws

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using WebsocketTest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+app.UseWebSockets();
 
 var summaries = new[]
 {
@@ -26,6 +29,21 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
+});
+app.Map("/ws", async (context) =>
+{
+    if (!context.WebSockets.IsWebSocketRequest)
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return;
+    }
+
+    using var ws = await context.WebSockets.AcceptWebSocketAsync("wss");
+    while (true)
+    {
+        await ws.WriteMessage("Hello world");
+        await Task.Delay(1000);
+    }
 });
 
 app.Run();
---
+++ SocketExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebsocketTest;
+
+public static class SocketExtensions
+{
+    public static async Task<string> ReadMessage(this WebSocket ws)
+    {
+        var buffer = new byte[512];
+        byte[] fullMessage = [];
+        WebSocketReceiveResult result;
+
+        do
+        {
+            result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            fullMessage = fullMessage.Concat(buffer).ToArray();
+        }
+        while (!result.EndOfMessage);
+
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            throw new WebSocketException("The websocket was closed");
+        }
+
+        var message = Encoding.UTF8.GetString(fullMessage);
+        return message;
+    }
+
+    public static async Task WriteMessage(this WebSocket ws, string message)
+    {
+        var bytes = Encoding.UTF8.GetBytes(message);
+        await ws.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    public static async Task<T> ReadObject<T>(this WebSocket ws)
+    {
+        var message = await ws.ReadMessage();
+        var obj = JsonConvert.DeserializeObject<T>(message);
+        return obj;
+    }
+
+    public static async Task WriteObject<T>(this WebSocket ws, T obj)
+    {
+        var json = JsonConvert.SerializeObject(obj);
+        await ws.WriteMessage(json);
+    }
+}
